ed69abfe43b42e243e6abadf455e6e8177936b35
qfx@yonyou.com <qfx@yonyou.com>
2019-12-19 17:17:51 +0800

    init

---
+++ base-eureka/src/test/java/com/jackvc/baseeureka/BaseEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackvc.baseeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
