59a1447bf4cfd6ba7319fcaab57e5f3246a25e54
qfx@yonyou.com <qfx@yonyou.com>
2019-12-19 20:21:33 +0800

    init

---
+++ base-zuul/src/test/java/com/jackvc/basezuul/BaseZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackvc.basezuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
